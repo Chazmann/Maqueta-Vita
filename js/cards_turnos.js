0bae78ced435408b915082df0786b770dbd65624
chazmann <lchaz85@gmail.com>
2024-11-05 11:28:29 -0300

    carga medicos

--- js/cards_turnos.js
+++ js/cards_turnos.js
@@ -35,11 +35,14 @@ $(document).ready(function () {
 
     // Días de la semana
     const weekdays = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
-
+    $("#availableAppointments").append(`<h4>Resultados de Turnos Disponibles para <b>${specialtyValue}</b></h4>`);
     // Generar 4 cards aleatorias
     for (let i = 0; i < 4; i++) {
-      // Seleccionar un médico aleatorio
-      const doctorName = doctors[Math.floor(Math.random() * doctors.length)];
+      // Seleccionar un médico aleatorio y eliminarlo de la lista de médicos
+      const doctorIndex = Math.floor(Math.random() * doctors.length);
+      const doctorName = doctors[doctorIndex];
+      doctors.splice(doctorIndex, 1); // Eliminar el médico seleccionado de la lista
+
 
       // Seleccionar una sede aleatoria
       const location = locations[Math.floor(Math.random() * locations.length)];
@@ -56,20 +59,20 @@ $(document).ready(function () {
       // Generar un rango horario aleatorio entre las 8 y las 17hs
       const startHour = Math.floor(Math.random() * 10) + 8; // Entre 8 y 17
       const endHour = startHour + 3; // Rango de 3 horas
-
+     
       // Crear la card con los datos generados
       const card = `
-                <div class="col-md-3 mb-4">
+                <div class="col-md-6 mb-4 col-xs-12">
                     <div class="card">
                         <div class="card-body row">
                             <div class="col-9">
                                 <h5 class="card-title">${specialtyValue}</h5>
                                 <h3 class="card-text fs-4"><b>${doctorName}</b></h3>
                                 <p class="fs-5"><em>${location}</em></p>
-                                <p class="card-text"><strong>Días:</strong> ${selectedDays.join(
+                                <p class="card-text fs-6"><strong>Días:</strong> ${selectedDays.join(
                                   ", "
                                 )}</p>
-                                <p class="card-text"><strong>Horario:</strong> ${startHour}:00 - ${endHour}:00</p>
+                                <p class="card-text fs-6"><strong>Horario:</strong> ${startHour}:00 - ${endHour}:00</p>
                             </div>
                             <div class="col-3">
                                 <i class="fa fa-info btn btn-secondary p-2 w-100 rounded-5"></i>
@@ -81,6 +84,7 @@ $(document).ready(function () {
             `;
 
       // Insertar la card en el contenedor #availableAppointments
+      
       $("#availableAppointments").append(card);
     }
   });
